9859bf478d92ff63b7da18b5167fd5730172c7c2
kevin.claassen <claassenk@outlook.com>
2021-05-01 23:07:25 +0200

    Ongoing: Fixed accessing Language and Currency value
    Trip:
    Fixed observer method, all entered data is now showing in detail screen
    Changed data class Trip to have dateFrom and dateTo as String value
    Added dates and data binding in Detail Fragment and data binding in NewTripFragment.kt
    Started implementing Room and Dao
    
    Main:
    Added bottom_navigation_menu.xml
    Implemented Bottom Navigation bar, but navigation not working correctly as yet

--- app/src/main/java/com/example/android/capstoneproject_aroundtheworld/MainActivity.kt
+++ app/src/main/java/com/example/android/capstoneproject_aroundtheworld/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.android.capstoneproject_aroundtheworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -9,21 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_nav_menu)
-        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        val navView: BottomNavigationView = findViewById(R.id.bottom_nav_menu)
+
+        val navController: NavController = findNavController(R.id.myNavHostFragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                        R.id.bottom_menu_countries,
+                        R.id.bottom_menu_trips
+                )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+//        val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_nav_menu)
+//        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.bottom_menu_countries -> {
-
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.bottom_menu_trips -> {
-
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+//    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//        when (item.itemId) {
+//            R.id.bottom_menu_countries -> {
+//
+//                return@OnNavigationItemSelectedListener true
+//            }
+//            R.id.bottom_menu_trips -> {
+//
+//                return@OnNavigationItemSelectedListener true
+//            }
+//        }
+//        false
+//    }
 }
