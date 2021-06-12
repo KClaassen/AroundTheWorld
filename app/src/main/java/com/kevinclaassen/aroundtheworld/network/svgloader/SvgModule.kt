0a4b1ec945a32807cc0637e9b3ae95403861569d
kevin.claassen <claassenk@outlook.com>
2021-06-12 10:36:36 +0200

    TripDetailFragment.kt: Working on adding MotionLayout and onClick for expanding selected image on click

--- app/src/main/java/com/kevinclaassen/aroundtheworld/network/svgloader/SvgModule.kt
+++ app/src/main/java/com/kevinclaassen/aroundtheworld/network/svgloader/SvgModule.kt
@@ -2,7 +2,9 @@ package com.kevinclaassen.aroundtheworld.network.svgloader
 
 import android.content.Context
 import android.graphics.drawable.PictureDrawable
+import android.util.Log
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
@@ -23,5 +25,9 @@ class SvgModule : AppGlideModule() {
     // Disable manifest parsing to avoid adding similar modules twice.
     override fun isManifestParsingEnabled(): Boolean {
         return false
+    }
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        builder.setLogLevel(Log.VERBOSE)
     }
 }
--- app/src/main/java/com/kevinclaassen/aroundtheworld/trips/detail/TripDetailFragment.kt
+++ app/src/main/java/com/kevinclaassen/aroundtheworld/trips/detail/TripDetailFragment.kt
@@ -24,6 +24,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
@@ -118,30 +119,35 @@ class TripDetailFragment : Fragment(), ImageListAdapter.ImageListListener, Image
     override fun onImageClick(imagePath: String) {
         Log.i("expand image", "expand image clicked")
         Glide.with(requireActivity())
-            .load(imagePath)
-            .listener(object: RequestListener<Drawable> {
-                override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        isFirstResource: Boolean
-                ): Boolean {
-                    return false
-                }
+                .load(imagePath)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .listener(object: RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                    ): Boolean {
+                        e?.printStackTrace()
+                        return false
+                    }
 
-                override fun onResourceReady(
-                        resource: Drawable?,
-                        model: Any?,
-                        target: Target<Drawable>?,
-                        dataSource: DataSource?,
-                        isFirstResource: Boolean
-                ): Boolean {
-                    return false
-                }
-            })
+                    override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                    ): Boolean {
+                        binding.tripDetailLayout.transitionToStart()
+                        binding.tripDetailLayout.transitionToEnd()
+                        return false
+                    }
+                })
                 .into(expanded_trip_view_image)
-        binding.tripDetailLayout.transitionToStart()
-        binding.tripDetailLayout.transitionToEnd()
+
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
